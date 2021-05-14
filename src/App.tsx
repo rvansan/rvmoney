95d7e9f01b969930f122e340d8441c3e9a9e19f3
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-05-14 01:11:36 -0300

    01- Configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+  setIsNewTransactionModalOpen(false);
+  }
+
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <GlobalStyle />
+      <Modal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h1>Meu Modal</h1>
+      </Modal>
     </>
   );
 }
